13823e508589c19acad9efa6d0be6fda1959433d
praveen <napraveen03@gmail.com>
2023-12-23 01:07:33 +0530

    changed

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns=[
     path('stop_video/', views.stop_video, name='stop_video'),
     path('video/', views.video, name='video'),
     path('monitor', views.monitor, name='monitor'),
+    path('crack_detection', views.crack_detection, name='crack_detection'),
 ]
